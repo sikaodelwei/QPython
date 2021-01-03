5a5516eb0719741614e824b4365fe354f586f62e
sikaodelwei <liuxiaolong2014dream@gmail.com>
2021-01-03 15:45:55 +0800

    Delete 剪切板test.py

--- 剪切板test.py
+++
@@ -1,7 +0,0 @@
-#-*-coding:utf8-*-
-from androidhelper import Android
-droid = Android()
-droid.setClipboard("978751333286629787513335447197875596295861 ")
-
-clipboard = droid.getClipboard().result
-print(clipboard)
