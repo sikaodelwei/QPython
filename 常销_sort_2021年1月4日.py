ccc9a0d71c42f257c3d24b8d04972d8b01c79e77
sikaodelwei <liuxiaolong2014dream@gmail.com>
2021-01-28 03:18:58 +0800

    Add files via upload

---
+++ 常销_sort_2021年1月4日.py
@@ -0,0 +1,27 @@
+#-*-coding:utf8-*-
+import xlrd
+data = xlrd.open_workbook("../Download/WeiXin/cf.xls")
+table = data.sheet_by_name('cf')
+rows = table.nrows
+list=[]
+listv=[]
+for row in range(0,rows):
+      cla=table.cell(row,4).value.encode('utf-8')
+
+      bookname=table.cell(row,2).value.encode('utf-8')
+      bz=table.cell(row,11).value.encode('utf-8')  
+      bz_start=bz[0:1] 
+  
+      if bz_start=="C":
+         list.append(bz[0:6]+bookname)
+         
+
+for cb in list:
+    for j in range(0,len(list)):
+     if cb[0:1]=="C":
+         listv.append(cb)
+             
+    
+for cb in list:
+    print(cb)
+   
